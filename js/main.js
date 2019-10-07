4e5c0c07f6e1aa4cc7b7e1e2d5a9c835ba5e08b6
Ivan Parkhomenka <bdfybujhtdbx@gmail.com>
2019-10-07 12:30:43 +0300

    Плавность анимации попапа, доработка карты

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var link = document.querySelector(".search-btn");
 var popup = document.querySelector(".form-search");
+var dateIn = popup.querySelector("[name=date-in]");
 
 popup.classList.add("form-search-hide");
 link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.toggle("form-search-show");
+    dateIn.focus();
 })
